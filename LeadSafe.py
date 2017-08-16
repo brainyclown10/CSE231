ea898abca110aa4b526e9e38a62fc7adf81967b7
Brandon Hu <brandon2009731@gmail.com>
2017-08-15 20:43:02 -0400

    Changed a small typo and formatting issues.

--- LeadSafe.py
+++ LeadSafe.py
@@ -5,26 +5,25 @@ points_ahead_int = int(points_str)
 # 2. Subtract Three
 lead_calculation_float = float(points_ahead_int - 3)
 
-#3, Add a half-point if the team that is ahead has the ball and subtract a half-point if the other team has the ball
+# 3, Add a half-point if the team that is ahead has the ball and subtract a half-point if the other team has the ball
 has_ball_str = input("Does the lead team have the ball (Yes or No): ")
 
 if has_ball_str == "Yes":
-  lead_calculation_float = lead_calculation_float + 0.5
+    lead_calculation_float = lead_calculation_float + 0.5
 else:
-  lead_calculation_float = load_calculation_float - 0.5
+    lead_calculation_float = lead_calculation_float - 0.5
 
 # (Numbers less than zero become zero)
 if lead_calculation_float < 0:
-  lead_calculation_float = 0
+    lead_calculation_float = 0
 
 # 4. Square that
-lead_calculation_float = lead_calculation_float **2
+lead_calculation_float = lead_calculation_float ** 2
 
 # 5. If the result is greater than the number of seconds left in the game, the lead is safe
 seconds_remaining_int = int(input("Enter the number of seconds remaining: "))
 
 if lead_calculation_float > seconds_remaining_int:
-  print("Lead is safe.")
-else
-  print("Lead is not safe.")
-  
+    print("Lead is safe.")
+else:
+    print("Lead is not safe.")
