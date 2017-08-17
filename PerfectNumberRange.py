ba01663ead6c9ed738b2ea6f59e9f9ad49133577
Brandon Hu <brandon2009731@gmail.com>
2017-08-17 17:29:35 -0400

    Changed the Perfect Number Range example to use elif and else.

--- PerfectNumberRange.py
+++ PerfectNumberRange.py
@@ -4,7 +4,7 @@ top_num_str = input("What is the upper number for the range:")
 top_num = int(top_num_str)
 number = 2
 while number <= top_num:
-# sum the divisors of number
+    # sum the divisors of number
     divisor = 1
     sum_of_divisors = 0
     while divisor < number:
@@ -14,8 +14,8 @@ while number <= top_num:
 # classify the number based on its divisor sum
     if number == sum_of_divisors:
         print(number, "is perfect")
-    if number < sum_of_divisors:
+    elif number < sum_of_divisors:
         print(number, "is abundant")
-    if number > sum_of_divisors:
+    else:
         print(number, "is deficient")
     number += 1
